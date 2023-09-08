6f8beaf655226286ccd7590e01dd3651d932d61b
Mary <omota4fidelity@gmail.com>
2023-09-08 21:11:42 +0100

    in progress

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -3,23 +3,38 @@ import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
 
-function ShoppingList({ items }) {
-  const [selectedCategory, setSelectedCategory] = useState("All");
 
-  function handleCategoryChange(event) {
-    setSelectedCategory(event.target.value);
+function ShoppingList({ items }) {
+  const [selectedCategory, setSelectedCategory] = useState("All")
+  const [selectedName, setSelectedName] = useState("")
+  const [currentEventTarget, setCurrentEventTarget] = useState("filter")
+
+  function handleCategoryChange(e) {
+    setSelectedCategory(e.target.value)
+    setCurrentEventTarget(e.target.name)
   }
 
-  const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
+function handleSearchChange(e){
+  setSelectedName(e.target.value)
+  setCurrentEventTarget(e.target.name)
+}
 
-    return item.category === selectedCategory;
+  const itemsToDisplay = items.filter((item) => {
+    if (currentEventTarget === "search"){
+    if (selectedCategory === "") return true
+
+    return item.name.toLowerCase().includes(selectedName.toLowerCase())
+    } else if(currentEventTarget === "filter") {
+      if(selectedCategory === "All") return true
+      return item.category === selectedCategory
+    }
+    
   });
 
   return (
     <div className="ShoppingList">
       <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <Filter onCategoryChange={handleCategoryChange} onSearchChange={handleSearchChange}/>
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
